622c49e5949d1445616bb5c8b73f18edf0884144
Sotiris <sotgasotga@gmail.com>
2025-06-05 23:01:09 +0200

    Cchanged ListenBrainz URL

--- plugins/ListenBrainz/src/ListenBrainz.ts
+++ plugins/ListenBrainz/src/ListenBrainz.ts
@@ -6,7 +6,7 @@ type NowPlayingPayload = Omit<Payload, "listened_at">;
 export class ListenBrainz {
 	private static async sendRequest(body?: { listen_type: "single" | "playing_now"; payload: Payload[] | NowPlayingPayload[] }) {
 		if (storage.userToken === "") throw new Error("User token not set");
-		return fetch(`https://api.listenbrainz.org/1/submit-listens`, {
+		return fetch(`https://maloja.sotga.de/apis/listenbrainz/1/submit-listens`, {
 			headers: {
 				"Content-type": "application/json",
 				Accept: "application/json",
